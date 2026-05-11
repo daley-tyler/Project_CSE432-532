2cedcfd4437f97cfa268f4b4440b81814cafb751
daleytn@miamioh.edu <daleytn@miamioh.edu>
2026-05-11 19:46:36 -0400

    More work on logistical. Added empty files for gaussian and knn which will be next.

--- SER_Project/minilearn/classifiers/logistical_regression.py
+++ SER_Project/minilearn/classifiers/logistical_regression.py
@@ -1,11 +1,20 @@
 import numpy as np
 
 class LogisticRegression:
-    def __init__(self):
-        pass
+    def __init__(self, learning_rate=0.1, n_iter=500):
+        self.learning_rate = learning_rate
+        self.n_iter = n_iter
+        self.class_ = None
+        self.weight_ = None
+
     
-    def fit():
-        pass
+    def sigmoid(self, z):
+        return np.exp(z) / (1 + np.exp(z))
+
+    def fit(self, X, y):
+        X = np.array(X, dtype=float)
+        y = np.array(y)
+        
         
     def predict():
         pass
