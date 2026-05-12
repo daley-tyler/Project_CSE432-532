7f8bb594b150d91ade61552a124939e09e78e613
daleytn@miamioh.edu <daleytn@miamioh.edu>
2026-05-11 22:29:05 -0400

    Finished KNN Classifier.

--- SER_Project/minilearn/classifiers/knn.py
+++ SER_Project/minilearn/classifiers/knn.py
@@ -10,3 +10,29 @@ class KNNClassifier:
         self.X_train_ = np.array(X, dtype=float)
         self.y_train_ = np.array(y)
         return self
+    
+    def predict_r(self, row):
+        diff = self.X_train_ - row
+        squared_distances = np.sum(diff ** 2, axis=1)
+        distance = np.sqrt(squared_distances)
+        near_inds = np.argsort(distance)[:self.n_neighbors_]
+        near_labs = self.Y_train_[near_inds]
+
+        labels, counts = np.unique(near_labs, return_counts=True)
+        b_idx = np.argmax(counts)
+        return labels[b_idx]
+    
+    def predict(self,X):
+        X = np.array(X, dtype=float)
+        predicts = []
+
+        for row in X:
+            predicts.append(self.predict_r(row))
+        
+        return np.array(predicts)
+    
+    def score(self, X, y):
+        predicts = self.predict(X)
+        return np.mean(predicts == np.array(y))
+    
+KNN = KNNClassifier
