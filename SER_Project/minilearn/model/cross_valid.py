7ef79d8176bc0e1c06b10081f1b99643954365c7
daleytn@miamioh.edu <daleytn@miamioh.edu>
2026-05-12 12:01:15 -0400

    Added build for each split.

--- SER_Project/minilearn/model/cross_valid.py
+++ SER_Project/minilearn/model/cross_valid.py
@@ -16,3 +16,22 @@ def k_fold_split(y, n_splits=5, shuffle=True):
         for i in range(len(c_idx)):
             f_num = i % n_splits
             folds[f_num].append(c_idx[i])
+
+    splits = []
+    row_idx = np.arrange(len(y))
+
+    for j in range(n_splits):
+        valid_idx = np.array(folds[j], dtype=int)
+
+        train_rows = np.ones(len(y), dtype=bool)
+        train_rows[valid_idx] = False
+
+        train_idx = row_idx[train_rows]
+
+        if shuffle == True:
+            np.random.shuffle[train_idx]
+            np.random.shuffle[valid_idx]
+
+        splits.append((train_idx, valid_idx))
+
+    return splits
