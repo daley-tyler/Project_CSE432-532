e4ee3f724989cdb06115de8171298acd73ac0cb7
daleytn@miamioh.edu <daleytn@miamioh.edu>
2026-05-12 04:37:38 -0400

    Forgot to stage

--- SER_Project/minilearn/metrics/__init__.py
+++ SER_Project/minilearn/metrics/__init__.py
@@ -1,3 +1,3 @@
-from .metric import (accuracy_score)
+from .metric import accuracy_score, confusion_matrix, precision_score, recall_score, f1_score
 
-__all__ = ["accuracy_score"]
+__all__ = ["accuracy_score", "confusion_matrix", "precision_score", "recall_score", "f1_score"]
