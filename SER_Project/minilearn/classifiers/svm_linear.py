dbedfcca2c002670f36bd9f45b67bf126768a299
daleytn@miamioh.edu <daleytn@miamioh.edu>
2026-05-12 01:04:18 -0400

    Adding code to linear svm.

--- SER_Project/minilearn/classifiers/svm_linear.py
+++ SER_Project/minilearn/classifiers/svm_linear.py
@@ -1,5 +1,13 @@
 import numpy as np
 
 class LinearSVM:
-    def __init__():
-        pass
+    def __init__(self, learning_rate=0.001, n_iter=500, C=1.0):
+        self.learning_rate = learning_rate
+        self.n_iter = n_iter
+        self.C_= C
+
+    def add_bias(self, X):
+        X = np.array(X, dtype=float)
+        ones = np.ones((X.shape[0], 1))
+        return np.column_stack((ones,X))
+    
