d58dc44b56d6cd137f451245b9638933349b6b4f
daleytn@miamioh.edu <daleytn@miamioh.edu>
2026-05-12 23:49:38 -0400

    ann fit completed

--- SER_Project/minilearn/model/ann.py
+++ SER_Project/minilearn/model/ann.py
@@ -24,3 +24,49 @@ class ANNClassifier:
         z = z - np.max(z, axis=1, keepdims=True)
         e = np.exp(z)
         return e / np.sum(e, axis=1, keepdims=True)
+    
+    def fit(self, X, y):
+        X = np.array(X, dtype=float)
+        y = np.array(y)
+
+        self.classes_, y_idx = np.unique(y, return_inverse=True)
+        self.class_ = self.classes_
+
+        n_rows = X.shape[0]
+        n_features = X.shape[1]
+        n_classes = len(self.classes_)
+
+        y_one = np.zeros((n_rows, n_classes))
+        y_one[np.arange(n_rows), y_idx] = 1
+
+        rng = np.random.default_rng(self.random_state)
+        self.W1_ = rng.normal(0, np.sqrt(2 / n_features), size=(n_features, self.hidden_size))
+        self.b1_ = np.zeros(self.hidden_size)
+        self.W2_ = rng.normal(0, np.sqrt(2 / self.hidden_size), size=(self.hidden_size, n_classes))
+        self.b2_ = np.zeros(n_classes)
+        self.loss_ = []
+
+        for i in range(self.n_iter):
+            z1 = X.dot(self.W1_) + self.b1_
+            a1 = self.relu(z1)
+            z2 = a1.dot(self.W2_) + self.b2_
+            probs = self.softmax(z2)
+
+            loss = -np.mean(np.log(probs[np.arange(n_rows), y_idx] + 1e-12))
+            self.loss_.append(loss)
+
+            dz2 = (probs - y_one) / n_rows
+            dW2 = a1.T.dot(dz2)
+            db2 = np.sum(dz2, axis=0)
+
+            da1 = dz2.dot(self.W2_.T)
+            dz1 = da1 * self.relu_grad(z1)
+            dW1 = X.T.dot(dz1)
+            db1 = np.sum(dz1, axis=0)
+
+            self.W2_ = self.W2_ - self.learning_rate * dW2
+            self.b2_ = self.b2_ - self.learning_rate * db2
+            self.W1_ = self.W1_ - self.learning_rate * dW1
+            self.b1_ = self.b1_ - self.learning_rate * db1
+
+        return self
